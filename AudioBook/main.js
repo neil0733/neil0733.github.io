7043d22c1f1e51464529e01c8d2534bd41207fef
Neil0733 <neil060802@gmail.com>
2022-06-27 09:13:05 +0800

    a

--- AudioBook/main.js
+++ AudioBook/main.js
@@ -7,7 +7,7 @@ let playpause_btn = document.querySelector(".playpause-track");
 let next_btn = document.querySelector(".next-track");
 let prev_btn = document.querySelector(".prev-track");
 
-let seek_slider = document.querySelector(".seek_slider");
+let seek_slider = document.querySelector("#seek_slider");
 let volume_slider = document.querySelector(".volume_slider");
 let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
